5fac0af5128e37de2551a34e9a0c13559bec9091
Muhammed Ali SUSLU <malisuslu@yandex.com>
2022-07-28 22:51:54 +0300

    ...

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -3,8 +3,8 @@ import TableHeading from "./TableHeading";
 
 function Table() {
   return (
-    <div className="md:max-h-[60vh] max-h-[50vh] md:max-w-[40vw] max-w-[90vw] rounded-lg bg-white overflow-x-hidden overflow-y-auto scrollbar">
-      <table className="md:border-x-[40px] border-x-[20px] border-slate-400 table-fixed w-full border-separate border-spacing-0 break-all">
+    <div className="md:max-h-[80vh] max-h-[44vh] md:max-w-[40vw] max-w-[90vw] rounded-lg bg-slate-400 md:px-10 px-4 overflow-x-hidden overflow-y-auto scrollbar">
+      <table className="table-fixed w-full border-separate border-spacing-0 break-all">
         <TableHeading />
         <TableBody />
       </table>
